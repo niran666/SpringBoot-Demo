ef4b75d8acdf92ef4b8068296a4efe63c44878f1
niran666 <1291614525@qq.com>
2019-03-01 12:18:14 +0800

    实现简单登录

---
+++ SpringBoot/SpringSecurity-1/src/main/java/com/example/demo/se/WebSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.example.demo.se;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyAuthenctiationSuccessHandler myAuthenctiationSuccessHandler;		//认证成功处理类
+    @Autowired
+    MyAuthenctiationFailureHandler myAuthenctiationFailureHandler;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                System.out.println(charSequence.toString());
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return s.equals(charSequence.toString());
+            }
+        });
+    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                // 如果有允许匿名的url，填在下面
+////                .antMatchers().permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                // 设置登陆页
+//                .formLogin().loginPage("/login")
+//                // 设置登陆成功页
+//                .defaultSuccessUrl("/").permitAll()
+//                // 自定义登陆用户名和密码参数，默认为username和password
+////                .usernameParameter("username")
+////                .passwordParameter("password")
+//                .and()
+//                .logout().permitAll();
+//
+//        // 关闭CSRF跨域
+//        http.csrf().disable();
+//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .successHandler(myAuthenctiationSuccessHandler) // 自定义登录成功处理
+                .failureHandler(myAuthenctiationFailureHandler)
+                .and()
+            .logout()
+                .permitAll();
+        http.csrf().disable();
+
+    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER");
+//    }
+}
